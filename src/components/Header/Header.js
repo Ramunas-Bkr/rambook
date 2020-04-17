eccd8eb06bc1def4ffcdb141020ba8dff62a92dd
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-04-17 12:30:40 +0300

    Components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="app-head">
+            <img src="https://i.pinimg.com/originals/d0/54/89/d054890aa6a20fe5273d24feff7acc79.jpg" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="hero-photo">
+                <img src="https://www.mountain-forecast.com/images/mtn_home.jpg" alt="photo" />
+            </div>
+            <div>
+                Avatar + description
+            </div>
+            <div>
+                My posts
+            <div>
+                New posts
+            </div>
+            </div>
+            <div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+            </div>
+      </div>
+    )
+}
+
+export default Profile
