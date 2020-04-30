12d7d92cf7c5a67a25697bb2c28973f850c4687d
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-04-30 09:09:34 +0300

    Users API

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import classes from './Users.module.css'
+import Axios from 'axios';
+import profileImage from '../../assets/images/empty_avatar.jpg'
+
 
 const Users = (props) => {
+
+    if (props.users.length === 0) {
+        Axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
+    }
 
     return (
         <div>
@@ -10,7 +20,7 @@ const Users = (props) => {
                     <div key={u.id} className={classes.Users}>
                         <div className={classes.left}>
                             <div className={classes.avatar}>
-                                <img src={u.profilePicture} alt='profile' />
+                                <img src={u.photos.small != null ? u.photos.small : profileImage} alt='profile' />
                             </div>
                             {u.isFrend
                                 ? <button onClick={() => props.unfollow(u.id)} >Unfollow</button>
@@ -18,10 +28,10 @@ const Users = (props) => {
                             }
                         </div>
                         <div className={classes.right}>
-                            <div className={classes.nickname}>{u.nickName}</div>
-                            <div className={classes.city}>{u.location.city}</div>
-                            <div className={classes.name}>{u.firstName} {u.lastName}</div>
-                            <div className={classes.country}>{u.location.country}</div>
+                            <div className={classes.nickname}>{u.name}</div>
+                            <div className={classes.city}>{"u.location.city"}</div>
+                            <div className={classes.name}>{"u.firstName"} {"u.lastName"}</div>
+                            <div className={classes.country}>{"u.location.country"}</div>
                             <div className={classes.status}>{u.status}</div>
                         </div>
 
--- src/redux/usersReducers.js
+++ src/redux/usersReducers.js
@@ -4,58 +4,58 @@ const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        {
-            id: 1,
-            isFrend: true,
-            profilePicture: 'https://avatarfiles.alphacoders.com/547/54799.png',
-            nickName: 'Ramas',
-            firstName: 'Ramūnas',
-            lastName: 'Bekeris',
-            status: 'Mokausi React. Geras dalykas',
-            location: {
-                city: 'Vilnius',
-                country: 'Lithuania'
-            }
-        },
-        {
-            id: 2,
-            isFrend: false,
-            profilePicture: 'https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-02-2-512.png',
-            nickName: 'Elzauskas',
-            firstName: 'Elzė',
-            lastName: 'Bekerytė',
-            status: 'Mokausi daugybos lentelės',
-            location: {
-                city: 'Vilnius',
-                country: 'Lithuania'
-            }
-        },
-        {
-            id: 3,
-            isFrend: true,
-            profilePicture: 'https://cdn1.vectorstock.com/i/1000x1000/80/40/cute-and-funny-monster-avatar-animated-cartoon-vector-15028040.jpg',
-            nickName: 'Zivile S.',
-            firstName: 'Živilė',
-            lastName: 'Šulskaitė',
-            status: 'Angaras turi kilti',
-            location: {
-                city: 'Vilnius',
-                country: 'Lithuania'
-            }
-        },
-        {
-            id: 4,
-            isFrend: true,
-            profilePicture: 'https://www.bestfunnies.com/wp-content/uploads/2015/05/Funny-Dog-faces-27.jpg',
-            nickName: 'Janis Oz',
-            firstName: 'Janis',
-            lastName: 'Ozuolnis',
-            status: 'Laiks ne gaidis, banks pagaidis',
-            location: {
-                city: 'Ventspils',
-                country: 'Latvia'
-            }
-        },
+        // {
+        //     id: 1,
+        //     isFrend: true,
+        //     profilePicture: 'https://avatarfiles.alphacoders.com/547/54799.png',
+        //     nickName: 'Ramas',
+        //     firstName: 'Ramūnas',
+        //     lastName: 'Bekeris',
+        //     status: 'Mokausi React. Geras dalykas',
+        //     location: {
+        //         city: 'Vilnius',
+        //         country: 'Lithuania'
+        //     }
+        // },
+        // {
+        //     id: 2,
+        //     isFrend: false,
+        //     profilePicture: 'https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-02-2-512.png',
+        //     nickName: 'Elzauskas',
+        //     firstName: 'Elzė',
+        //     lastName: 'Bekerytė',
+        //     status: 'Mokausi daugybos lentelės',
+        //     location: {
+        //         city: 'Vilnius',
+        //         country: 'Lithuania'
+        //     }
+        // },
+        // {
+        //     id: 3,
+        //     isFrend: true,
+        //     profilePicture: 'https://cdn1.vectorstock.com/i/1000x1000/80/40/cute-and-funny-monster-avatar-animated-cartoon-vector-15028040.jpg',
+        //     nickName: 'Zivile S.',
+        //     firstName: 'Živilė',
+        //     lastName: 'Šulskaitė',
+        //     status: 'Angaras turi kilti',
+        //     location: {
+        //         city: 'Vilnius',
+        //         country: 'Lithuania'
+        //     }
+        // },
+        // {
+        //     id: 4,
+        //     isFrend: true,
+        //     profilePicture: 'https://www.bestfunnies.com/wp-content/uploads/2015/05/Funny-Dog-faces-27.jpg',
+        //     nickName: 'Janis Oz',
+        //     firstName: 'Janis',
+        //     lastName: 'Ozuolnis',
+        //     status: 'Laiks ne gaidis, banks pagaidis',
+        //     location: {
+        //         city: 'Ventspils',
+        //         country: 'Latvia'
+        //     }
+        // },
     ]
 };
 
@@ -85,7 +85,7 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS:
             return {
                 ...state, 
-                users: [state.users, action.users]
+                users: [...state.users, ...action.users]
             }
         default:
             return state
