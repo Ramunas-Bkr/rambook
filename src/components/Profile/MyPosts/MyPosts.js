87fe5ce03cf0f6f7c2e1b5d9b629c2026336968a
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-05-26 14:17:51 +0300

    React.memo

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,33 +1,27 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import classes from './MyPosts.module.css'
-import PostReduxForm from './MyPostsForm'
 import Post from './Post/Post'
-export class MyPosts extends PureComponent {
+import PostReduxForm from './MyPostsForm'
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     return nextProps !== this.props || nextState !== this.state
-    // }
+const MyPosts = React.memo(props => {
 
-    render() {
+    const postElement = props.posts.map( p => 
+    <Post message={p.message}
+        likecount={p.likecount}
+    /> )
 
-        const postElement = this.props.posts.map(p =>
-            <Post message={p.message}
-                likecount={p.likecount}
-            />)
-
-        let addPost = (value) => {
-            this.props.addPost(value.addMyPost);
-        }
-
-        return (
-            <div className={classes.MyPosts}>
-                <h3 className={classes.postHeader}> My posts </h3>
-                <PostReduxForm
-                    onSubmit={addPost} />
-                {postElement}
-            </div>
-        )
+    let addPost = (value) => {
+        props.addPost(value.addMyPost);
     }
-}
+
+    return (
+        <div className={classes.MyPosts}>
+            <h3 className={classes.postHeader}> My posts </h3>
+            <PostReduxForm 
+                onSubmit={addPost}/>
+            {postElement}
+        </div>
+    )
+})
 
 export default MyPosts
